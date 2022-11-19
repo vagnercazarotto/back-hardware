15a4fcede0ac7cf6ce59fad9d394f27980a600c8
VAGNER CAZAROTTO <vagner.cazarotto@t-systems.com>
2022-11-19 17:14:07 -0300

    run

--- src/main/java/com/ucs/hardware/hardwaredata/HardwaredataApplication.java
+++ src/main/java/com/ucs/hardware/hardwaredata/HardwaredataApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class HardwaredataApplication {
 
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(HardwaredataApplication.class, args);
 	}
